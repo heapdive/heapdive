8f1829406906b6c39bd4edbf81843566507306c6
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-09-12 15:24:12 +0900

    move values to entries

--- intellij-community-hprof/src/main/kotlin/com/intellij/diagnostic/hprof/parser/HProfVisitor.kt
+++ intellij-community-hprof/src/main/kotlin/com/intellij/diagnostic/hprof/parser/HProfVisitor.kt
@@ -19,132 +19,131 @@ import java.nio.ByteBuffer
 
 open class HProfVisitor {
 
-  private val myTopLevelVisits = BooleanArray(RecordType.HeapDumpEnd.value + 1)
-  private val myHeapDumpVisits = BooleanArray(HeapDumpRecordType.RootUnknown.value + 1)
+    private val myTopLevelVisits = BooleanArray(RecordType.HeapDumpEnd.value + 1)
+    private val myHeapDumpVisits = BooleanArray(HeapDumpRecordType.RootUnknown.value + 1)
 
-  lateinit var visitorContext: VisitorContext
+    lateinit var visitorContext: VisitorContext
 
-  val heapRecordOffset: Long
-    get() = visitorContext.currentHeapRecordOffset
+    val heapRecordOffset: Long
+        get() = visitorContext.currentHeapRecordOffset
 
-  val sizeOfID: Int
-    get() = visitorContext.idSize
+    val sizeOfID: Int
+        get() = visitorContext.idSize
 
-  init {
-    enableAll()
-  }
-
-  fun isEnabled(type: RecordType): Boolean {
-    return myTopLevelVisits[type.value]
-  }
-
-  fun isEnabled(type: HeapDumpRecordType): Boolean {
-    return myHeapDumpVisits[type.value]
-  }
-
-  private fun enableAll() {
-    for (recordType in RecordType.values()) {
-      myTopLevelVisits[recordType.value] = true
+    init {
+        enableAll()
     }
-    for (recordType in HeapDumpRecordType.values()) {
-      myHeapDumpVisits[recordType.value] = true
+
+    fun isEnabled(type: RecordType): Boolean {
+        return myTopLevelVisits[type.value]
     }
-  }
 
-  fun disableAll() {
-    for (recordType in RecordType.values()) {
-      myTopLevelVisits[recordType.value] = false
+    fun isEnabled(type: HeapDumpRecordType): Boolean {
+        return myHeapDumpVisits[type.value]
     }
-    for (recordType in HeapDumpRecordType.values()) {
-      myHeapDumpVisits[recordType.value] = false
+
+    private fun enableAll() {
+        for (recordType in RecordType.entries) {
+            myTopLevelVisits[recordType.value] = true
+        }
+        for (recordType in HeapDumpRecordType.entries) {
+            myHeapDumpVisits[recordType.value] = true
+        }
     }
-  }
 
-  fun enable(type: RecordType) {
-    if (type === RecordType.HeapDump || type === RecordType.HeapDumpSegment) {
-      myTopLevelVisits[RecordType.HeapDump.value] = true
-      myTopLevelVisits[RecordType.HeapDumpSegment.value] = true
+    fun disableAll() {
+        for (recordType in RecordType.entries) {
+            myTopLevelVisits[recordType.value] = false
+        }
+        for (recordType in HeapDumpRecordType.entries) {
+            myHeapDumpVisits[recordType.value] = false
+        }
     }
-    else {
-      myTopLevelVisits[type.value] = true
+
+    fun enable(type: RecordType) {
+        if (type === RecordType.HeapDump || type === RecordType.HeapDumpSegment) {
+            myTopLevelVisits[RecordType.HeapDump.value] = true
+            myTopLevelVisits[RecordType.HeapDumpSegment.value] = true
+        } else {
+            myTopLevelVisits[type.value] = true
+        }
     }
-  }
 
-  fun enable(type: HeapDumpRecordType) {
-    enable(RecordType.HeapDump)
-    myHeapDumpVisits[type.value] = true
-  }
+    fun enable(type: HeapDumpRecordType) {
+        enable(RecordType.HeapDump)
+        myHeapDumpVisits[type.value] = true
+    }
 
-  fun disable(type: RecordType) {
-    myTopLevelVisits[type.value] = false
-  }
+    fun disable(type: RecordType) {
+        myTopLevelVisits[type.value] = false
+    }
 
-  fun disable(type: HeapDumpRecordType) {
-    myHeapDumpVisits[type.value] = false
-  }
+    fun disable(type: HeapDumpRecordType) {
+        myHeapDumpVisits[type.value] = false
+    }
 
-  open fun preVisit() {}
-  open fun postVisit() {}
+    open fun preVisit() {}
+    open fun postVisit() {}
 
-  open fun visitStringInUTF8(id: Long, s: String) {}
-  open fun visitLoadClass(classSerialNumber: Long, classObjectId: Long, stackSerialNumber: Long, classNameStringId: Long) {}
+    open fun visitStringInUTF8(id: Long, s: String) {}
+    open fun visitLoadClass(classSerialNumber: Long, classObjectId: Long, stackSerialNumber: Long, classNameStringId: Long) {}
 
-  open fun visitStackFrame(stackFrameId: Long,
-                           methodNameStringId: Long,
-                           methodSignatureStringId: Long,
-                           sourceFilenameStringId: Long,
-                           classSerialNumber: Long,
-                           lineNumber: Int) {
-  }
+    open fun visitStackFrame(stackFrameId: Long,
+                             methodNameStringId: Long,
+                             methodSignatureStringId: Long,
+                             sourceFilenameStringId: Long,
+                             classSerialNumber: Long,
+                             lineNumber: Int) {
+    }
 
-  open fun visitStackTrace(stackTraceSerialNumber: Long,
-                           threadSerialNumber: Long,
-                           numberOfFrames: Int,
-                           stackFrameIds: LongArray) {
-  }
+    open fun visitStackTrace(stackTraceSerialNumber: Long,
+                             threadSerialNumber: Long,
+                             numberOfFrames: Int,
+                             stackFrameIds: LongArray) {
+    }
 
-  // TODO: Many of these are not implemented yet. Events are fired, but there are no parameters
-  open fun visitAllocSites() {}
+    // TODO: Many of these are not implemented yet. Events are fired, but there are no parameters
+    open fun visitAllocSites() {}
 
-  open fun visitHeapSummary(totalLiveBytes: Long, totalLiveInstances: Long, totalBytesAllocated: Long, totalInstancesAllocated: Long) {}
-  open fun visitStartThread() {}
-  open fun visitEndThread(threadSerialNumber: Long) {}
-  open fun visitHeapDump() {}
-  open fun visitHeapDumpEnd() {}
-  open fun visitCPUSamples() {}
-  open fun visitControlSettings() {}
+    open fun visitHeapSummary(totalLiveBytes: Long, totalLiveInstances: Long, totalBytesAllocated: Long, totalInstancesAllocated: Long) {}
+    open fun visitStartThread() {}
+    open fun visitEndThread(threadSerialNumber: Long) {}
+    open fun visitHeapDump() {}
+    open fun visitHeapDumpEnd() {}
+    open fun visitCPUSamples() {}
+    open fun visitControlSettings() {}
 
-  open fun visitRootUnknown(objectId: Long) {}
-  open fun visitRootGlobalJNI(objectId: Long, jniGlobalRefId: Long) {}
-  open fun visitRootLocalJNI(objectId: Long, threadSerialNumber: Long, frameNumber: Long) {}
-  open fun visitRootJavaFrame(objectId: Long, threadSerialNumber: Long, frameNumber: Long) {}
-  open fun visitRootNativeStack(objectId: Long, threadSerialNumber: Long) {}
-  open fun visitRootStickyClass(objectId: Long) {}
-  open fun visitRootThreadBlock(objectId: Long, threadSerialNumber: Long) {}
-  open fun visitRootMonitorUsed(objectId: Long) {}
-  open fun visitRootThreadObject(objectId: Long, threadSerialNumber: Long, stackTraceSerialNumber: Long) {}
+    open fun visitRootUnknown(objectId: Long) {}
+    open fun visitRootGlobalJNI(objectId: Long, jniGlobalRefId: Long) {}
+    open fun visitRootLocalJNI(objectId: Long, threadSerialNumber: Long, frameNumber: Long) {}
+    open fun visitRootJavaFrame(objectId: Long, threadSerialNumber: Long, frameNumber: Long) {}
+    open fun visitRootNativeStack(objectId: Long, threadSerialNumber: Long) {}
+    open fun visitRootStickyClass(objectId: Long) {}
+    open fun visitRootThreadBlock(objectId: Long, threadSerialNumber: Long) {}
+    open fun visitRootMonitorUsed(objectId: Long) {}
+    open fun visitRootThreadObject(objectId: Long, threadSerialNumber: Long, stackTraceSerialNumber: Long) {}
 
-  open fun visitPrimitiveArrayDump(
-    arrayObjectId: Long, stackTraceSerialNumber: Long,
-    numberOfElements: Long,
-    elementType: Type,
-    primitiveArrayData: ByteBuffer) {
-  }
+    open fun visitPrimitiveArrayDump(
+            arrayObjectId: Long, stackTraceSerialNumber: Long,
+            numberOfElements: Long,
+            elementType: Type,
+            primitiveArrayData: ByteBuffer) {
+    }
 
-  open fun visitClassDump(
-    classId: Long,
-    stackTraceSerialNumber: Long,
-    superClassId: Long,
-    classloaderClassId: Long,
-    instanceSize: Long,
-    constants: Array<ConstantPoolEntry>,
-    staticFields: Array<StaticFieldEntry>,
-    instanceFields: Array<InstanceFieldEntry>) {
-  }
+    open fun visitClassDump(
+            classId: Long,
+            stackTraceSerialNumber: Long,
+            superClassId: Long,
+            classloaderClassId: Long,
+            instanceSize: Long,
+            constants: Array<ConstantPoolEntry>,
+            staticFields: Array<StaticFieldEntry>,
+            instanceFields: Array<InstanceFieldEntry>) {
+    }
 
-  open fun visitObjectArrayDump(arrayObjectId: Long, stackTraceSerialNumber: Long, arrayClassObjectId: Long, objects: LongArray) {}
+    open fun visitObjectArrayDump(arrayObjectId: Long, stackTraceSerialNumber: Long, arrayClassObjectId: Long, objects: LongArray) {}
 
-  open fun visitInstanceDump(objectId: Long, stackTraceSerialNumber: Long, classObjectId: Long, bytes: ByteBuffer) {}
+    open fun visitInstanceDump(objectId: Long, stackTraceSerialNumber: Long, classObjectId: Long, bytes: ByteBuffer) {}
 
-  open fun visitUnloadClass(classSerialNumber: Long) {}
+    open fun visitUnloadClass(classSerialNumber: Long) {}
 }
