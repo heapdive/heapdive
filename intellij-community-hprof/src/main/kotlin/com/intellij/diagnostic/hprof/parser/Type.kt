5773eae9b086f338def3c16553d57a3e49943c5f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-09-12 15:24:46 +0900

    move values to entries

--- intellij-community-hprof/src/main/kotlin/com/intellij/diagnostic/hprof/parser/Type.kt
+++ intellij-community-hprof/src/main/kotlin/com/intellij/diagnostic/hprof/parser/Type.kt
@@ -17,45 +17,44 @@
 package com.intellij.diagnostic.hprof.parser
 
 import org.jetbrains.annotations.NonNls
-import java.util.*
 
 enum class Type(val typeId: Int, val size: Int, @NonNls private val arrayName: String) {
-  // Pointer sizes are dependent on the hprof file, so set it to 0 for now.
-  OBJECT(2, 0, ""),
-  BOOLEAN(4, 1, "[Z"),
-  CHAR(5, 2, "[C"),
-  FLOAT(6, 4, "[F"),
-  DOUBLE(7, 8, "[D"),
-  BYTE(8, 1, "[B"),
-  SHORT(9, 2, "[S"),
-  INT(10, 4, "[I"),
-  LONG(11, 8, "[J");
+    // Pointer sizes are dependent on the hprof file, so set it to 0 for now.
+    OBJECT(2, 0, ""),
+    BOOLEAN(4, 1, "[Z"),
+    CHAR(5, 2, "[C"),
+    FLOAT(6, 4, "[F"),
+    DOUBLE(7, 8, "[D"),
+    BYTE(8, 1, "[B"),
+    SHORT(9, 2, "[S"),
+    INT(10, 4, "[I"),
+    LONG(11, 8, "[J");
 
-  fun getClassNameOfPrimitiveArray(): String {
-    if (this == OBJECT) {
-      throw IllegalArgumentException("OBJECT type is not a primitive type")
-    }
-    return arrayName
-  }
-
-  companion object {
-    private val sIdTypeMap = HashMap<Int, Type>()
-    private val sNameTypeMap = HashMap<String, Type>()
-
-    init {
-      for (type in values()) {
-        sIdTypeMap[type.typeId] = type
-        sNameTypeMap[type.arrayName] = type
-      }
+    fun getClassNameOfPrimitiveArray(): String {
+        if (this == OBJECT) {
+            throw IllegalArgumentException("OBJECT type is not a primitive type")
+        }
+        return arrayName
     }
 
-    fun getType(id: Int): Type {
-      return sIdTypeMap[id]!!
-    }
+    companion object {
+        private val sIdTypeMap = HashMap<Int, Type>()
+        private val sNameTypeMap = HashMap<String, Type>()
 
-    fun getType(name: String): Type {
-      return sNameTypeMap[name]!!
+        init {
+            for (type in entries) {
+                sIdTypeMap[type.typeId] = type
+                sNameTypeMap[type.arrayName] = type
+            }
+        }
+
+        fun getType(id: Int): Type {
+            return sIdTypeMap[id]!!
+        }
+
+        fun getType(name: String): Type {
+            return sNameTypeMap[name]!!
+        }
     }
-  }
 }
 
