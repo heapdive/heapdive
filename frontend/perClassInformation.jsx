2e60bf9d6873dfe81b18b0b2e651fbb237c088f5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-09-12 18:18:02 +0900

    better coloring

--- frontend/perClassInformation.jsx
+++ frontend/perClassInformation.jsx
@@ -16,19 +16,57 @@
 
 import React from "react";
 
-const HighlightText = ({text}) => {
-    const highlightedText = text.split('\n').map((line, index) => {
-        if (/ROOT: Static field.*CGLIB/.test(line) || /org.springframework.aop.framework/.test(line)) {
-            return <span key={index} className="muted">{line}<br/></span>;
-        } else if (/lettuce|mysql|jedis/.test(line)) {
-            return <span key={index} className="highlight">{line}<br/></span>;
+function processText(text) {
+    const lines = text.split('\n');
+    const newLines = lines.map((line, index) => {
+        if (/(ROOT: Static field.*CGLIB|org.springframework.aop.framework)/.test(line)) {
+            return <span key={index} className="muted">${line}<br/></span>;
         }
-        return <span key={index}>{line}<br/></span>;
+
+        const match = line.match(/]\s*([0-9.]+)([KMGTP]?B)/);
+        if (match) {
+            const sizeInBytes = convertToBytes(parseFloat(match[1]), match[2]);
+            const color = getColorBasedOnSize(sizeInBytes);
+            return <span key={index} style={{color: color}}>{line}<br/></span>;
+        } else {
+            return <span>{line}<br/></span>;
+        }
     });
 
-    return (
-        <pre>{highlightedText}</pre>
-    );
+    return <div>{newLines}</div>;
+}
+
+function convertToBytes(size, unit) {
+    const units = {
+        'B': 1,
+        'KB': 1e3,
+        'MB': 1e6,
+        'GB': 1e9,
+        'TB': 1e12,
+        'PB': 1e15,
+        'EB': 1e18,
+        'ZB': 1e21,
+        'YB': 1e24
+    };
+
+    return size * units[unit];
+}
+
+function getColorBasedOnSize(size) {
+    const MAX_SIZE = 100e6; // 100MB
+    const MIN_SIZE = 1e6;  // 1MB
+
+    if (size >= MAX_SIZE) return 'red';
+    if (size <= MIN_SIZE) return 'black';
+
+    // Gradient logic: linear gradient between black (0) and red (100)
+    const ratio = (size - MIN_SIZE) / (MAX_SIZE - MIN_SIZE);
+    const r = Math.min(255, Math.round(255 * ratio));
+    return `rgb(${r}, 0, 0)`;
+}
+
+const HighlightText = ({text}) => {
+    return processText(text);
 }
 
 export class PerClassInformation extends React.Component {
